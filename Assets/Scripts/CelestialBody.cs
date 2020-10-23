72514f523bce1e69b0aedac228edb7d6267308bc
Matthew Rousseau <matthew.rousseau@icloud.com>
2020-10-24 00:42:13 +0200

    Add 2D CelestialBody Simulation

---
+++ Assets/Scripts/CelestialBody.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CelestialBody : MonoBehaviour
+{
+    public float mass;
+    public float radius;
+    public Vector2 initialVelocity;
+    private Vector2 currentVelocity;
+
+
+    private Rigidbody2D rigidBody;
+
+    private bool has_collided = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Set transform and collider scale
+        transform.localScale = radius * Vector3.one;
+
+        rigidBody = GetComponent<Rigidbody2D>();
+
+        currentVelocity = initialVelocity;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("TRIGGER");
+        currentVelocity = Vector2.zero;
+        has_collided = true;
+    }
+
+    public void UpdateVelocity(CelestialBody[] allBodies, float timeDelta)
+    {
+        Vector2 force = Vector2.zero;
+        foreach (CelestialBody body in allBodies)
+        {
+            if (this == body)
+                continue;
+
+            Vector2 direction = body.rigidBody.position - rigidBody.position;
+            Vector2 forceDir = direction.normalized;
+            float sqrDist = direction.sqrMagnitude;
+
+            force += (Universe.gravitationalConstant * mass * body.mass / sqrDist) * forceDir;
+        }
+        if (!has_collided)
+            currentVelocity += timeDelta / mass * force;
+    }
+
+    public void UpdatePosition(float timeDelta)
+    {
+       rigidBody.position += timeDelta * currentVelocity;
+    }
+}
---
+++ Assets/Scripts/Universe.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Universe
+{
+    public static float gravitationalConstant = 6.67E-3F;
+}
